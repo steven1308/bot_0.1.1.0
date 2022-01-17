5e1031b12a9de3f0b8f669dbe21e3f311f45ddb9
steven1308 <75292814+steven1308@users.noreply.github.com>
2022-01-17 18:49:55 +0800

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = new SlashCommandBuilder()
 	.setName('list')
-	.setDescription('音樂清單')
+	.setDescription('音樂清單，預設是第一頁')
 	.addIntegerOption(option =>
 		option.setName('頁數')
 		.setDescription('預設是第一頁'));
