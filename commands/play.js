2c2ebdb3d1ac064867493b0c8e724f39154a0e44
Quasi <quasipersonal58@gmail.com>
2022-03-17 13:35:21 +0800

    合併預設播放和播放

--- commands/play.js
+++ commands/play.js
@@ -3,7 +3,13 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = new SlashCommandBuilder()
 	.setName('play')
 	.setDescription('撥放音樂')
-	.addStringOption(option => option
-		.setName('網址')
-		.setDescription('輸入歌曲或歌單的網址')
-		.setRequired(true));
+	.addStringOption(option =>
+		option.setName('網址')
+			.setDescription('輸入歌曲或歌單的網址'))
+	.addStringOption(option =>
+		option.setName('預設')
+			.setDescription('選取清單名稱')
+			.addChoice('test', 'https://www.youtube.com/playlist?list=PLW2qsjqU0e5fzjM_O3PpuSa81F4Sb0K35')
+			.addChoice('動畫歌曲', 'https://www.youtube.com/playlist?list=PLW2qsjqU0e5cjheTzIy7VeBfdsYSG1mIp')
+			.addChoice('A', 'https://www.youtube.com/playlist?list=PLW2qsjqU0e5csx4PiaSTJ7XA2mhd7Tqnc')
+	);
