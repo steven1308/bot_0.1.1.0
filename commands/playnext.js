23f34c6dfb514689b7155e98ef8280358b4b2ffe
Quasi <quasipersonal58@gmail.com>
2022-03-21 11:17:21 +0800

    Flx ZodError

--- commands/playnext.js
+++ commands/playnext.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = new SlashCommandBuilder()
-	.setName('playNext')
+	.setName('playnext')
 	.setDescription('加入歌曲到第一首')
 	.addStringOption(option =>
 		option.setName('網址')
